1032cfb7443a6532ee2d8477c4119527ecd6749f
김준수 <asdg2795@naver.com>
2025-05-10 23:32:02 +0900

    Feat: 사용자 엔티티에 고유 ID 필드 추가

--- src/main/java/com/kkiri_trip/back/domain/user/dto/Response/LoginResponseDto.java
+++ src/main/java/com/kkiri_trip/back/domain/user/dto/Response/LoginResponseDto.java
@@ -8,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponseDto {
+    private Long id;
     private String email;
     private String nickname;
     private String profileUrl;

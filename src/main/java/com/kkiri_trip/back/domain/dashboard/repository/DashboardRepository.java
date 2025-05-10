bb0bf7a66e13d96c3eb72f3a3634c948ac3460a6
김준수 <asdg2795@naver.com>
2025-05-10 22:56:34 +0900

    Feat: 다른 사용자의 대시보드 조회

---
+++ src/main/java/com/kkiri_trip/back/domain/dashboard/repository/DashboardRepository.java
@@ -0,0 +1,13 @@
+package com.kkiri_trip.back.domain.dashboard.repository;
+
+import com.kkiri_trip.back.domain.dashboard.entity.Dashboard;
+import com.kkiri_trip.back.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DashboardRepository extends JpaRepository<Dashboard, Long>, DashboardRepositoryCustom {
+    Optional<Dashboard> findByUser(User user);
+}
